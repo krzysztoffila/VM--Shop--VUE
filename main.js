a4d7818bc090075ffe3186204a8c40db317814d1
krzysztoffila <umowy.fila@gmail.com>
2024-07-29 18:41:48 +0200

    Feature: Handling Event on variant color with change product image

--- main.js
+++ main.js
@@ -2,15 +2,24 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Socks",
+      cart: 0,
       description: "Fancy Socks with Vue logo",
       image: "./assets/images/socks_green.jpg",
       inventory: 100,
       onSale: false,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
+        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
       ],
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
