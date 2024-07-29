5a5ffe9f658e30412c3c3eb4ac89baa89e59f89d
krzysztoffila <umowy.fila@gmail.com>
2024-07-29 18:34:57 +0200

    Feature: Rendering list with variants and details

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const app = Vue.createApp({
       description: "Fancy Socks with Vue logo",
       image: "./assets/images/socks_green.jpg",
       inventory: 100,
+      onSale: false,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
     };
   },
 });
